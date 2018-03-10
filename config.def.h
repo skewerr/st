1f6bf56c7e9af77346da818208c8598d5d510314
spoonm <spoonm@spoonm.org>
2018-03-10 10:16:39 -0300

    blergh

--- config.def.h
+++ config.def.h
@@ -183,7 +183,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ TERMMOD,              XK_I,           iso14755,       {.i =  0} },
-	{ MODKEY,               XK_U,           externalpipe,   {.v =  openurlcmd} },
+	{ TERMMOD,              XK_U,           externalpipe,   {.v =  openurlcmd} },
 };
 
 /*
