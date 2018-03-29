9d730fb50a370fd49d773943169d25acdae385c5
spoonm <spoonm@spoonm.org>
2018-03-28 23:02:59 -0300

    updated config

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVu Sans Mono:size=9:antialias=true:autohint=true";
+static char *font = "DejaVu Sans Mono:size=8.8:antialias=true:autohint=true";
 static int borderpx = 5;
 
 /*
